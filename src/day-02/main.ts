5d3679ad713f44739b76fe08b0a4908c11293ed1
Guillaume ROUSSIN <groussin@sigma.fr>
2024-12-04 09:02:05 +0100

    Day 02 Part 2

--- src/day-02/main.ts
+++ src/day-02/main.ts
@@ -1,13 +1,13 @@
 import { loadInput } from "@util/main";
 
 // Parse phase
-const reports = loadInput(__dirname, true).map((line) =>
+const reports = loadInput(__dirname).map((line) =>
   line.split(/\s+/g).map((el) => Number.parseInt(el)),
 );
 
 // Part 1
 
-function isReportSafe(report: number[]): { safe: boolean; i: number | null } {
+function isReportSafe(report: number[]): boolean {
   let old_slope_sign: number | null = null;
 
   for (let i = 1; i < report.length; i++) {
@@ -15,34 +15,34 @@ function isReportSafe(report: number[]): { safe: boolean; i: number | null } {
     const slope_val = Math.abs(val);
     const slope_sign = val / Math.abs(val);
 
-    if (slope_val < 1 || slope_val > 3) return { safe: false, i: i - 1 };
-    if (old_slope_sign != null && old_slope_sign != slope_sign)
-      return { safe: false, i: i - 1 };
+    if (slope_val < 1 || slope_val > 3) return false;
+    if (old_slope_sign != null && old_slope_sign != slope_sign) return false;
 
     old_slope_sign = slope_sign;
   }
 
-  return { safe: true, i: null };
+  return true;
 }
 
 // reports.forEach((report) => {
 //   console.log(report, isReportSafe(report) ? "Safe" : "Unsafe");
 // });
 
-const num_safe_reports = reports.filter(
-  (report) => isReportSafe(report).safe,
-).length;
+const num_safe_reports = reports.filter(isReportSafe).length;
 console.log("Part 1:", num_safe_reports);
 
 // Part 2
 
 function badLevelFilter(report: number[]): boolean {
-  const { safe, i } = isReportSafe(report);
-  if (safe) return true;
+  if (isReportSafe(report)) return true;
 
-  // REmove bad level from array and try again
-  const new_report = report.filter((_, index) => index != i);
-  return isReportSafe(new_report).safe;
+  // Remove bad level from array and try again
+  for (let i = 0; i < report.length; i++) {
+    const new_report = report.filter((_, index) => index != i);
+    if (isReportSafe(new_report)) return true;
+  }
+
+  return false;
 }
 
 reports.forEach((report) => {
